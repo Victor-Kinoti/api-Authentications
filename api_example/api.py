1b2142c92775b7671f53605efbc7da3273f70629
Victor-Kinoti <vickeynote@gmail.com>
2018-07-26 01:44:59 +0300

    complete code

--- api_example/api.py
+++ api_example/api.py
@@ -164,6 +164,76 @@ def login():
 	return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
 
 
+@app.route('/todo', methods=['GET'])
+@token_required
+def get_all_todos(current_user):
+	todos = Todo.query.filter_by(user_id=current_user.id).all()
+
+	output = []
+
+	for todo in todos:
+		todo_data = {}
+		todo_data['id'] = todo.id
+		todo_data['text'] = todo.text
+		todo_data['complete'] = todo.complete
+		output.append(todo_data)
+
+	return jsonify({'todos' : output})
+
+
+@app.route('/todo/<todo_id>', methods=['GET'])
+@token_required
+def get_one_todo(current_user, todo_id):
+	todo = Todo.query.filter_by(id=todo_id, user_id = current_user.id).first()
+
+	if not todo:
+		return jsonify({'message' : 'no todo found!'})
+
+	todo_data = {}
+	todo_data['id'] = todo.id
+	todo_data['text'] = todo.text
+	todo_data['complete'] = todo.complete
+
+
+	return jsonify(todo_data)
+
+
+@app.route('/todo', methods=['POST'])
+@token_required
+def create_todo(current_user):
+	data = request.get_json()
+	#print (type(data))
+	new_todo = Todo(text=data['text'], complete=False, user_id=current_user.id)
+	db.session.add(new_todo)
+	db.session.commit()
+	return jsonify({'message' : 'todo created!'})
+
+
+@app.route('/todo/<todo_id>', methods=['PUT'])
+@token_required
+def complete_todo(current_user, todo_id):
+	todo = Todo.query.filter_by(id=todo_id, user_id = current_user.id).first()
+
+	if not todo:
+		return jsonify({'message' : 'no todo found!'})
+
+	todo.complete = True
+	db.session.commit()
+
+	return jsonify({'message' : 'todo completed'})
+
+@app.route('/todo/<todo_id>', methods=['DELETE'])
+@token_required
+def delete_todo(current_user, todo_id):
+	todo = Todo.query.filter_by(id=todo_id, user_id=current_user.id).first()
+
+	if not todo:
+		return jsonify({'message' : 'No todo found!'})
+
+	db.session.delete(todo)
+	db.session.commit()
+
+	return jsonify({'message' : 'todo deleted'})
 
 if __name__ == '__main__':
 
