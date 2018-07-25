6416c7156bc32ab06a12dceb0155717080e29b31
Victor-Kinoti <vickeynote@gmail.com>
2018-07-25 03:09:48 +0300

    admin authorizations and normal user restrictions

--- api_example/api.py
+++ api_example/api.py
@@ -4,6 +4,7 @@ import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -25,11 +26,40 @@ class Todo(db.Model):
 	complete = db.Column(db.Boolean)
 	user_id = db.Column(db.Integer)
 
+def token_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		token = None
+
+		if 'x-access-token' in request.headers:
+			token = request.headers['x-access-token']
+
+		if not token:
+			return jsonify({'message' : 'Token missing!'}), 401
+
+		try:
+			data = jwt.decode(token, app.config['SECRET_KEY'])
+			current_user = User.query.filter_by(public_id=data['public_id']).first()
+		except:
+			return jsonify({'message' : 'Token invalid!'}), 401
+
+
+		return f(current_user, *args, **kwargs)
+	return decorated
+
+
 
 @app.route('/user', methods=['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+
+	if not current_user.admin:
+		return jsonify({'message' : 'cannot perform that function!'})
+
 	users = User.query.all()
+
 	output = []
+
 	for user in users:
 		user_data = {}
 		user_data['public_id'] = user.public_id
@@ -41,7 +71,12 @@ def get_all_users():
 	return jsonify({'users' : output})
 
 @app.route('/user/<public_id>', methods=['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user, public_id):
+
+	if not current_user.admin:
+		return jsonify({'message' : 'cannot perform that function!'})
+
 	user = User.query.filter_by(public_id=public_id).first()
 
 	if not user:
@@ -57,7 +92,12 @@ def get_one_user(public_id):
 
 
 @app.route('/user', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
+
+	if not current_user.admin:
+		return jsonify({'message' : 'cannot perform that function!'})
+
 	data = request.get_json() ##passes json data to variable called data
 
 	hashed_password = generate_password_hash(data['password'], method='sha256')
@@ -71,7 +111,12 @@ def create_user():
 
 
 @app.route('/user/<public_id>', methods=['PUT'])
-def promote_user(public_id):
+@token_required
+def promote_user(current_user, public_id):
+
+	if not current_user.admin:
+		return jsonify({'message' : 'cannot perform that function!'})
+
 	user = User.query.filter_by(public_id=public_id).first()
 
 	if not user:
@@ -82,7 +127,12 @@ def promote_user(public_id):
 	return jsonify({'message' : 'user promoted'})
 
 @app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
+
+	if not current_user.admin:
+		return jsonify({'message' : 'cannot perform that function!'})
+	
 	user = User.query.filter_by(public_id=public_id).first()
 
 	if not user:
